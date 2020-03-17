b83d556e74afe70a1fc5a819b01069936f281a3f
zixuan75 <wzixuanCC@gmail.com>
2020-03-17 18:45:13 -0400

    add text area

--- src/viewer/SecondWindow.java
+++ src/viewer/SecondWindow.java
@@ -1,18 +1,25 @@
 package viewer;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 public class SecondWindow extends JPanel {
 	private static JFrame frame = new JFrame("Usage");
+	private static JTextArea textArea=new JTextArea(25, 60);
 	public SecondWindow() {
 		JPanel panel = new JPanel();
+		Font font=new Font("DejaVu Sans", 5, 12);
+    	textArea.setFont(font);
+    	panel.add(textArea);
 		add(panel, BorderLayout.CENTER);
 	}
 	public void showWindow() {
 		frame.add(new SecondWindow());
     	frame.setVisible(true);
-    	frame.setSize(1200, 550);
+    	frame.setSize(790, 260);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
